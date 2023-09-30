0c4f02f3cb35228a9cfc7187859fa3b17357f6a8
iabdulb <abdulbasitm53@gmail.com>
2023-09-30 21:14:07 +0500

    youtube project

---
+++ script.js
@@ -0,0 +1,6 @@
+var menuicon = document.querySelector{".menu.png"};
+var sidebar = document.querySelector{".siderbar"};
+
+menuicon.onclick = function (){
+    sidebar.classlist.toggle("small-sidebar");
+}
